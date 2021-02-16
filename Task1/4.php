81eb0bf58b8550f7d5fcceda53c4c1d800d01b2a
Maverick88xx <maverick88xx@gmail.com>
2021-02-16 15:05:08 +0200

    Update 4.php

--- Task1/4.php
+++ Task1/4.php
@@ -5,9 +5,9 @@ function checkIP ($user_ip, $ip_begin, $ip_end)
     return (ip2long($user_ip) >= ip2long($ip_begin) && ip2long($user_ip) <= ip2long($ip_end));
 }
 
-//наш IP
+// наш IP
 $user_ip = "2.1.0.0";
 
-//диапазон адресов
+// диапазон адресов
 echo checkIP($user_ip, '1.127.255.100', '128.0.0.0') ? 'IP внутри подсети' : 'Нет';
 ?>
