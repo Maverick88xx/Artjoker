7798cf861dc28802ca2ade2e4d7aa257d8a473fb
Maverick88xx <maverick88xx@gmail.com>
2021-02-16 14:32:20 +0200

    Create 2.php

---
+++ Task1/2.php
@@ -0,0 +1,16 @@
+<?php
+//сколько чисел Фибоначчи выводим на экран
+$quantity = readline("Enter the number = ");
+
+function fibonacci($n, $first = 0, $second = 1)
+{
+    $fib = [$first, $second];
+    for($i = 1; $i < $n; $i++){
+        $fib[] = $fib[$i] + $fib[$i-1];
+    }
+    return $fib;
+}
+
+//выводим числа Фибоначчи
+print_r (fibonacci($quantity));
+?>
