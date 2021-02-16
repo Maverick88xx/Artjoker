38deaefbd32d590ae58c5759ab90c76590b94b84
Maverick88xx <maverick88xx@gmail.com>
2021-02-16 15:12:49 +0200

    Update 2.php

--- Task1/2.php
+++ Task1/2.php
@@ -5,7 +5,7 @@ $quantity = readline("Enter the number = ");
 function fibonacci($n, $first = 0, $second = 1)
 {
     $fib = [$first, $second];
-    for($i = 1; $i < $n; $i++){
+    for ($i = 1; $i < $n; $i++) {
         $fib[] = $fib[$i] + $fib[$i-1];
     }
     return $fib;
