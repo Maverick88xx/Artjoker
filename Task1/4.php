2e9d732bad3f40b5e39967fd50188b601697cba0
Maverick88xx <maverick88xx@gmail.com>
2021-02-16 14:56:33 +0200

    Create 4.php

---
+++ Task1/4.php
@@ -0,0 +1,13 @@
+<?php
+// проверка IP по маске
+function checkIP ($user_ip, $ip_begin, $ip_end) 
+{
+    return (ip2long($user_ip) >= ip2long($ip_begin) && ip2long($user_ip) <= ip2long($ip_end));
+}
+
+//наш IP
+$user_ip = "2.1.0.0";
+
+//диапазон адресов
+echo checkIP($user_ip, '1.127.255.100', '128.0.0.0') ? 'IP внутри подсети' : 'Нет';
+?>
